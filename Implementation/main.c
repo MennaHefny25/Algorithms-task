3d387a618951294cfb1332b10e75eafd4d2e4400
Menna Hefny <Menna Hefny>
2023-04-29 22:41:25 +0300

    algo task

---
+++ Implementation/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+int climbStairs(int n);
+int main()
+{
+    int n,numberofways=0;
+    numberofways = 1+(n/2);
+    scanf("%d",&n);
+    printf("%d\n",numberofways);
+    printf("%d\n",climbStairs(n));
+
+    return 0;
+}
+
+int climbStairs(int n) {
+    if(n == 0 || n == 1)
+        return 1;
+
+    int numberOfWays[n + 1];
+    numberOfWays[0] = 1;
+    numberOfWays[1] = 1;
+
+    for(int i = 2; i <= n ;i++) {
+        numberOfWays[i] = numberOfWays[i - 1] + numberOfWays[i - 2];
+    }
+    return numberOfWays[n];
+}
+
+
